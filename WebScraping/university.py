d84e564c8db74c7e3d3de3a184fcf5fef5d85b6b
patelvini <patelvini136@gmail.com>
2020-02-27 01:09:18 +0530

    Change : Update the university example

--- WebScraping/university.py
+++ WebScraping/university.py
@@ -1,19 +1,47 @@
 import re
 
-test_string = "XYZ:MUMBAI UNIVERSITYCREF:PUNE UNIVERSITYBREF:DADAR UNIVERSITYRREF:KOLHAPUR UNIVERCITY LLCREF:SOLAPUR UNIVERSITY"
+string_data = "XYZ:MUMBAI UNIVERSITYCREF:PUNE UNIVERSITYBREF:DADAR UNIVERSITYRREF:KOLHAPUR UNIVERCITY LLCREF:SOLAPUR UNIVERSITY"
 
-regex = r"?:(?<!L)[A-Z]?)REF|^(?!\|)"
-subst = r"||\g<0>"
-result = re.sub(regex, subst, test_string)
+regx = "(XYZ|CREF|BREF|RREF|REF):[a-zA-Z\\s]+?(LLC)?(?=(XYZ|CREF|BREF|RREF|REF)|$)"
 
+matches = re.finditer(regx, string_data)
+
+
+tag_list = []
+
+for match in matches:
+	tag_list.append(match.group())
+
+result= "|" + "|".join(tag_list)
 print(result)
 
 
-# tags = ['XYZ', 'CREF', 'BREF', 'RREF', 'REF']
-#
-# string_data = 'XYZ:MUMBAI UNIVERSITYCREF:PUNE UNIVERSITYBREF:DADAR UNIVERSITYRREF:KOLHAPUR UNIVERCITY LLCREF:SOLAPUR UNIVERSITY'
-#
-# for each_tag in tags:
-#     result = string_data.replace(each_tag, "|" + each_tag)
-#
+
+# regex = r"?:(?<!L)[A-Z]?)REF|^(?!\|)"
+# subst = r"||\g<0>"
+# result = re.sub(regex, subst, test_string)
+
 # print(result)
+
+#-----------------------------------------------------------
+
+# tags = ['XYZ', 'REF']
+
+# letters = ['C','B','R','']
+
+
+# for each_tag in tags:
+# 	if each_tag != 'REF':
+# 		string_data = string_data.replace(each_tag, "|" + each_tag)
+# 	else:
+# 		for j in letters:
+# 			string_data = string_data.replace(each_tag, "|" + j + each_tag)
+# 			break
+# 		break
+# 	continue
+
+
+
+
+
+# print(string_data)
