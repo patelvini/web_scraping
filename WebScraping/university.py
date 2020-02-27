010de53dfd1864436e5b1bedfe023696e0776bb5
Vini Patel <vini.patel@1rivet.local>
2020-02-27 17:39:41 +0530

    Add : Added code for university.py without using regx

--- WebScraping/university.py
+++ WebScraping/university.py
@@ -2,46 +2,36 @@ import re
 
 string_data = "XYZ:MUMBAI UNIVERSITYCREF:PUNE UNIVERSITYBREF:DADAR UNIVERSITYRREF:KOLHAPUR UNIVERCITY LLCREF:SOLAPUR UNIVERSITY"
 
-regx = "(XYZ|CREF|BREF|RREF|REF):[a-zA-Z\\s]+?(LLC)?(?=(XYZ|CREF|BREF|RREF|REF)|$)"
 
-matches = re.finditer(regx, string_data)
+tags = ['XYZ', 'REF']
+letters = ['C','B','R','']
+for each_tag in tags:
+	if each_tag != 'REF':
+		string_data = string_data.replace(each_tag, "|" + each_tag)
+	else:
+		for j in letters:
+			string_data = string_data.replace(each_tag, "|" + j + each_tag)
+			break
+	continue
+print(string_data)
 
 
-tag_list = []
-
-for match in matches:
-	tag_list.append(match.group())
-
-result= "|" + "|".join(tag_list)
-print(result)
 
 
 
-# regex = r"?:(?<!L)[A-Z]?)REF|^(?!\|)"
-# subst = r"||\g<0>"
-# result = re.sub(regex, subst, test_string)
 
+
+
+
+# regx = "(XYZ|[C|B|R]REF|REF):[a-zA-Z\\s]+?(LLC)?(?=(XYZ|[C|B|R]REF)|REF|$)"
+
+# matches = re.finditer(regx, string)
+
+# tag = re.findall(regx, string)
+# print(type(tag))
+
+# for match in matches:
+# 	tag_list.append(match.group())
+
+# result= "|" + "|".join(tag)
 # print(result)
-
-#-----------------------------------------------------------
-
-# tags = ['XYZ', 'REF']
-
-# letters = ['C','B','R','']
-
-
-# for each_tag in tags:
-# 	if each_tag != 'REF':
-# 		string_data = string_data.replace(each_tag, "|" + each_tag)
-# 	else:
-# 		for j in letters:
-# 			string_data = string_data.replace(each_tag, "|" + j + each_tag)
-# 			break
-# 		break
-# 	continue
-
-
-
-
-
-# print(string_data)
