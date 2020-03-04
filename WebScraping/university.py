a27fb0d504356a05465fa1d359035ff01efcaf03
patelvini <patelvini136@gmail.com>
2020-03-04 08:43:48 +0530

    Updated

--- WebScraping/university.py
+++ WebScraping/university.py
@@ -1,37 +1,12 @@
-import re
-
-string_data = "XYZ:MUMBAI UNIVERSITYCREF:PUNE UNIVERSITYBREF:DADAR UNIVERSITYRREF:KOLHAPUR UNIVERCITY LLCREF:SOLAPUR UNIVERSITY"
 
 
-tags = ['XYZ', 'REF']
-letters = ['C','B','R','']
-for each_tag in tags:
-	if each_tag != 'REF':
-		string_data = string_data.replace(each_tag, "|" + each_tag)
-	else:
-		for j in letters:
-			string_data = string_data.replace(each_tag, "|" + j + each_tag)
-			break
-	continue
+string_data = "XYZ:MUMBAI UNIVERSITYCREF:PUNE UNIVERSITYBREF:DADAR UNIVERSITYRREF:KOLHAPUR UNIVERCITY LLCREF:SOLAPUR UNIVERSITYREF:VALSAD"
+
+tags = ['XYZ','CREF','BREF','RREF']
+
+for i in range(0,len(tags)):
+	string_data = string_data.replace((tags[i]), "|" + tags[i],1)
+	x = string_data.find(tags[i])
+string_data = string_data[:x+4] + string_data[x+4:].replace('REF','|REF')
+
 print(string_data)
-
-
-
-
-
-
-
-
-
-# regx = "(XYZ|[C|B|R]REF|REF):[a-zA-Z\\s]+?(LLC)?(?=(XYZ|[C|B|R]REF)|REF|$)"
-
-# matches = re.finditer(regx, string)
-
-# tag = re.findall(regx, string)
-# print(type(tag))
-
-# for match in matches:
-# 	tag_list.append(match.group())
-
-# result= "|" + "|".join(tag)
-# print(result)
